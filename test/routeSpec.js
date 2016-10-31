9fa5571cf6ecd5d0cf46d57f91ac7e0d62cbec40
田杰 <jeff.tian@outlook.com>
2016-10-31 19:48:44 +0800

    Fix test.

--- test/routeSpec.js
+++ test/routeSpec.js
@@ -19,7 +19,7 @@ describe('Routes', function () {
     describe('GET /api/orders', function () {
         it('should return 200', function (done) {
             request()
-                .get('/api/registered')
+                .get('/api/orders')
                 .expect('Content-Type', /json/)
                 .expect(200, done);
         });
